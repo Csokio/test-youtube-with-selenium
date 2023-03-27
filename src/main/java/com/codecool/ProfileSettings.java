8217cb25b4aa444779de5c8f76ba2570c02547e6
Sőregi Krisztián <krisztiansoreg@gmail.com>
2023-03-27 12:23:28 +0200

    add files

---
+++ src/main/java/com/codecool/ProfileSettings.java
@@ -0,0 +1,12 @@
+package com.codecool;
+
+import org.openqa.selenium.WebDriver;
+
+public class ProfileSettings extends BasePage{
+
+    public ProfileSettings(
+    WebDriver driver)
+    {
+        super(driver);
+    }
+}
